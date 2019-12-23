38a47f161534f6e7bdb19a8230ee2bed01ef8ba2
Robert Thompson <rthom2171@icloud.com>
2019-12-22 20:40:00 -0600

    Added fields to ticket entity, set up local sqlite db

--- data/migrations/20191219201108_tickets.js
+++ data/migrations/20191219201108_tickets.js
@@ -2,9 +2,13 @@
 exports.up = function(knex, Promise) {
   return knex.schema.createTable('tickets', tbl => {
     tbl.increments(); // ticket number
-    tbl.integer('user_id').notNullable().references('id').inTable('users');
+    tbl.integer('user_id').notNullable().references('id').inTable('users'); // submitted by
     tbl.text('description', 500).notNullable();
     tbl.text('urgency').notNullable();
+    tbl.text('reply', 500);
+    tbl.boolean('solved').notNullable().defaultTo(0); // solved, defaults to false
+    tbl.text('category', 128).notNullable();
+    tbl.integer('solved_by').references('id').inTable('users');
   })
 };
 
