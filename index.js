988c8d86492e4f69f4164f988ea13f63f36913fb
indiMjc <45151346+indiMjc@users.noreply.github.com>
2019-12-24 15:05:07 -0500

    imported dotenv in index.js

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
+require('dotenv').config();
+
 const server = require('./server');
 
 const port = process.env.PORT || 4000;
 
 server.listen(port, () => {
-    console.log(`Server listening on port ${port}...`);
-})
+  console.log(`Server listening on port ${port}...`);
+});
