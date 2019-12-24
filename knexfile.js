f83eb4309c65cb5d85d24278901d623090d72eef
indiMjc <45151346+indiMjc@users.noreply.github.com>
2019-12-24 15:16:41 -0500

    changed migration file path and added credentials to knexfile

--- knexfile.js
+++ knexfile.js
@@ -1,7 +1,6 @@
 require('dotenv').config();
 
 module.exports = {
-
   development: {
     client: 'sqlite3',
     connection: './data/db/devdesk.db3',
@@ -15,7 +14,7 @@ module.exports = {
     client: 'postgresql',
     connection: {
       database: 'my_db',
-      user:     'username',
+      user: 'username',
       password: 'password'
     },
     pool: {
@@ -31,16 +30,15 @@ module.exports = {
     client: 'postgresql',
     connection: {
       database: 'my_db',
-      user:     'username',
-      password: 'password'
+      user: 'indimjc1@gmail.com',
+      password: process.env.PW
     },
     pool: {
       min: 2,
       max: 10
     },
     migrations: {
-      tableName: 'knex_migrations'
+      tableName: './data/migrations'
     }
   }
-
 };
